bc3cf1e19ddd710d6a7a6e1484bdeedc02f244bb
FelipeSantosRibas <felipesantosribas2007@gmail.com>
2025-11-28 14:25:35 -0300

    Adiciona funcionalidade para entrada de dados da conta e exibição de mensagem de confirmação

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,11 +1,31 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
         //TODO: Conhecer e importar a classe Scanner
-
         //Exibir as mensagens para o nosso usuário
-        
         //Obter pela scanner os valores digitados no terminal
-
         //Exibir a mensagem de conta criada
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Por favor, digite o número da conta: ");
+        numeroConta = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o nome do cliente: ");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, digite o saldo inicial: ");
+        saldo = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo R$ " + saldo + " já está disponível para saque.");
     }
 }
